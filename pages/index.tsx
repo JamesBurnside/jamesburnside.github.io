16d806af57845116a5eb13d529cd424761cf52ee
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-09-23 16:38:26 +0000

    Add post on triggering ADO pipeline from github actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   };
 };
 
-const testPosts: SerializedPostMetadata[] = Array(5).fill({
+const testPosts: SerializedPostMetadata[] = Array(4).fill({
   id: "test",
   title: "Test Page",
   abstract:
